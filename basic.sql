e99e7c7ebaff97268a7494e85b5741eae461d174
Kyle Zhao <removema@163.com>
2017-08-02 11:58:31 +0800

    create

--- basic.sql
+++ basic.sql
@@ -1,1 +1,26 @@
-basic sql command
+show databases();
+show tables();
+describe table_name;
+
+
+select version();
+select current_date();
+
+
+create database db_name;
+
+	create table pet 
+		(
+		 name varchar(20),
+		 owner varchar(20),
+		 species varchar(20),
+		 sex char(1),
+		 birth date,
+		 death date
+		);
+
+-- 从文本中批量导入数据,
+load data local infile 'data.txt' into table pet;
+
+
+
